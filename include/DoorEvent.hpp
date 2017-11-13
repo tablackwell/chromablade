67acad8fd20823070e5f70157f4f72c63fa313b9
Jason Kimko <jasonkimko@gmail.com>
2017-11-12 23:55:58 -0500

    Change Room to Level.

--- include/DoorEvent.hpp
+++ include/DoorEvent.hpp
@@ -3,14 +3,15 @@
 
 #include "EventInterface.hpp"
 #include "EventType.hpp"
-#include "Room.hpp"
+#include "Level.hpp"
 
 class DoorEvent : public EventInterface {
 public:
-    DoorEvent(Room room) { m_room = room; m_type = doorEvent; }
+    DoorEvent(Level level) { m_level = level; m_room = 0; m_type = doorEvent; }
     const EventType& getEventType(void) const { return m_type; };
 private:
-    Room m_room;
+    Level m_level;
+    int m_room;
     EventType m_type;
 };
 #endif
