e93c79fea74c7f6dc20a79033c99cfa0cf20714e
Liang <lwu03@email.wm.edu>
2017-11-26 14:43:48 -0500

    Adding color switching keys

--- include/KeySetting.hpp
+++ include/KeySetting.hpp
@@ -4,3 +4,6 @@
 #define UP sf::Keyboard::Up
 #define DOWN sf::Keyboard::Down
 #define ATTACK sf::Keyboard::Space
+#define RED sf::Keyboard::Num1
+#define BLUE sf::Keyboard::Num2
+#define YELLOW sf::Keyboard::Num3
